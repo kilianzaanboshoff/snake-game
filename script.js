ee08402d3a2d898a4627efa4e5e4d6aa0031e264
kilianboshoff <128136784+kilianzaanboshoff@users.noreply.github.com>
2023-12-31 18:45:57 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 // Define HTML Elements
-const board = document.getElementById('game-board')
+const board = document.getElementById('game-board');
+const instructionText = document.getElementById('instruction-text');
+const logo = document.getElementById('logo');
 
 // Define game variables
 let gridSize = 20;
 let snake = [{x: 10, y: 10}];
 let food = generateFood();
-let direction ='left';
+let direction ='up';
+let GameInterval;
+let gameSpeedDelay = 200;
+let gameStarted = false;
+
 
 
 
@@ -42,7 +48,7 @@ function setPosition(element, position){
 }
 
 // Testing draw function
-draw();
+// draw();
 
 
 // Draw food function
@@ -65,30 +71,127 @@ function generateFood() {
 function move(){
     const head = {...snake[0]}; //We are creating a copy of the snake head. but it is not changing the initial snake head position
     switch (direction) {
-        case 'right':
-            head.x ++; //We are increment the x position of the snake head by 1 
-            break;
         case 'up':
-            head.y --;
+            head.y--;
             break;
         case 'down':
-            head.y ++;
+            head.y++;
+            break;  
+         case 'left':
+            head.x--;
+            break;  
+        case 'right':
+            head.x++; //We are increment the x position of the snake head by 1 
             break;
-        case 'left':
-            head.x --;
-            break;
-
+    }
+       
         snake.unshift(head); //We are adding the new head to the snake array
+        //snake.pop(); //We are removing the last element of the snake array, it gives the illusion that the snake is moving
+    
+    if (head.x === food.x && head.y === food.y) {
+        food = generateFood(); //This means there will be a new food placed on the map
+        increaseSpeed();
+        clearInterval(GameInterval); //This will stop the interval
+        GameInterval = setInterval(() => {
+            move(); // move first
+            checkCollision();
+            draw(); // Then draw again new position
+        }, gameSpeedDelay);
+    }
+    else {
         snake.pop(); //We are removing the last element of the snake array, it gives the illusion that the snake is moving
     }
 }
 
 //Test move function
-setInterval(() => {
+// setInterval(() => {
 
-    move(); // move first
-    draw(); // Then draw again new position
+//     move(); // move first
+//     draw(); // Then draw again new position
 
-}, 200);
+// }, 200);
 
 
+// Start game function
+
+function StartGame () {
+    gameStarted = true; //keep track of the game state, whether it is running or not
+    instructionText.style.display = 'none';
+    logo.style.display = 'none';
+    GameInterval = setInterval(() => {
+        move(); // move first
+        checkCollision();
+        draw(); // Then draw again new position
+    }, gameSpeedDelay);
+
+}
+
+
+// Create a keypress event listener 
+
+function handleKeyPress(event) {
+    if (
+        (!gameStarted && event.code === 'Space') || 
+        (!gameStarted && event.key === ' ') 
+        ) {
+        StartGame();
+    } else {
+        switch (event.key) {
+            case 'ArrowUp':
+                direction = 'up';
+                break;
+            case 'ArrowDown':
+                direction = 'down';
+                break;
+            case 'ArrowLeft':
+                direction = 'left';
+                break;
+            case 'ArrowRight':
+                direction = 'right';
+                break;
+    }
+    }
+}
+
+document.addEventListener('keydown', handleKeyPress);
+
+function increaseSpeed() {
+    console.log(gameSpeedDelay);
+    if (gameSpeedDelay > 150 ){
+        gameSpeedDelay -= 5; //This will increase the speed of the snake
+    } else if (gameSpeedDelay > 100) {
+        gameSpeedDelay -= 3; //This will increase the speed of the snake
+    } else if (gameSpeedDelay > 50) {
+        gameSpeedDelay -= 2; //This will increase the speed of the snake
+    } else if (gameSpeedDelay > 25) {
+        gameSpeedDelay -= 1; //This will increase the speed of the snake
+    }
+}
+
+function checkCollision() {
+    const head  = snake [0]
+    if (head.x < 1 || head.x > gridSize || head.y < 1 || head.y > gridSize) {
+        resetGame();
+    }
+
+    for (let i = 1; i < snake.length; i++) {
+        if (head.x === snake[i].x && head.y === snake[i].y) {
+            resetGame();
+        }
+    }
+
+
+
+}
+
+function resetGame() {
+    snake = [{x: 10, y: 10}];
+    food = generateFood();
+    direction ='up';
+    gameSpeedDelay = 200;
+    clearInterval(GameInterval);
+    gameStarted = false;
+    instructionText.style.display = 'block';
+    logo.style.display = 'block';
+    draw();
+}
